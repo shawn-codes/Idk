90c5f2d4e46b46f0b62c96d954de873a9bf50c54
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2023-05-27 17:02:58 -0400

    Added dark mode function?

--- Function.js
+++ Function.js
@@ -1,2 +1,9 @@
-//scroll to top thingy
-const toTop = () => window.scrollTo({top:0, behavior: 'smooth'});
+//Adding dark mode functionality 
+
+function myFunction() {
+    var element = document.body; 
+    element.classList.toggle("dark mode"); 
+
+    //scroll to top thingy
+    const toTop = () => window.scrollTo({top:0, behavior: 'smooth'});
+}
